affcb99db44663f5313e790829187e15607be203
Maksim Gurbanow <maksim20051708@gmail.com>
2024-03-24 00:38:31 +0300

    add implementation for getCountWeekendsinMonth function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -185,8 +185,19 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let weekendCount = 0;
+  for (let day = 1; day <= 31; day += 1) {
+    const currentDate = new Date(year, month - 1, day);
+    if (currentDate.getMonth() !== month - 1) {
+      break;
+    }
+    if (currentDate.getDay() === 6 || currentDate.getDay() === 0) {
+      weekendCount += 1;
+    }
+  }
+
+  return weekendCount;
 }
 
 /**
